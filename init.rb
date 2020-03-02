56a74868df2d9d6c0d52a06be7644b42ba647b8f
bilel-kedidi <bilel.kedidi@gmail.com>
2020-03-02 14:45:02 +0100

    fix group

--- init.rb
+++ init.rb
@@ -10,6 +10,7 @@ class IssueGroupProjectHook < Redmine::Hook::ViewListener
   render_on :view_projects_form,   partial: 'hooks/projects/add_account_contact'
 end
 
+require 'redmine_creative_science/patches/controllers/issues_controller_patch'
 require 'redmine_creative_science/patches/helpers/projects_helper_patch'
 require 'redmine_creative_science/patches/models/time_entry_patch'
 require 'redmine_creative_science/patches/models/project_patch'
